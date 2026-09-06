4c99861ff0d494506cbc35a829b3d70a2cb2e008
Sree Varsha M <25it104@rmkec.ac.in>
2026-09-06 13:37:48 +0530

    Delete Downloads/weekend-wander directory

--- Downloads/weekend-wander/src/features/sort.js
+++
@@ -1,7 +0,0 @@
-import { qs } from "../utils/dom.js";
-import { setSortKey } from "../state.js";
-
-// FEATURE: Destination Sorting
-export function initSort() {
-  qs("#sort-select").addEventListener("change", (e) => setSortKey(e.target.value));
-}
--- Downloads/weekend-wander/src/utils/dom.js
+++
@@ -1,2 +0,0 @@
-export const qs = (sel, root = document) => root.querySelector(sel);
-export const qsa = (sel, root = document) => Array.from(root.querySelectorAll(sel));
